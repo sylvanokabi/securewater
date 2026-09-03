7445eb9f2c97d8fa1d75ec7ed65e9d20254792d4
sylvanokabi <mutombosylvano11@gmail.com>
2026-09-03 16:20:06 +0100

    refactor: ajout de l'architecture modulaire backend, frontend, simulateur et documentation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'utilisateurs',
+    'reservoirs',
+    'capteurs',
+    'alertes',
+    'temps_reel',
 ]
 
 MIDDLEWARE = [
