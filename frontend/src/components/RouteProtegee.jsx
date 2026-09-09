3b8857ee6ed6f9014cb184f5cf6b2da6b835634a
sylvanokabi <mutombosylvano11@gmail.com>
2026-09-09 17:47:03 +0100

    implementation des routes avec react-router-dom

---
+++ frontend/src/components/RouteProtegee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const RouteProtegee = () => {
+  const token = localStorage.getItem('access_token');
+
+  // Si le token est présent, on affiche les pages enfants, sinon redirection
+  return token ? <Outlet /> : <Navigate to="/connexion" replace />;
+};
+
+export default RouteProtegee;
